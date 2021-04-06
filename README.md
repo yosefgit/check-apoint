c174f425b6ebe13928664f8fcd82b032efbd3c6f
ygreiver <yosef.g@tevel-tech.com>
2021-04-06 16:05:59 +0300

    create script to download selenuim driver

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # check-apoint
-bot to check for appointments  
+bot to check for appointments
 
+dependancies:
+git
+npm
+node
+
+clone project:
+```
+git clone https://github.com/yosefgit/check-apoint.git
+```
+
+install and run:
 ```
 npm install
 npm start
---
+++ get-selenuim-driver.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ ! -f ~/drivers/geckodriver ]; then
+    echo "downloading driver..."
+    wget -P . https://github.com/mozilla/geckodriver/releases/download/v0.29.0/geckodriver-v0.29.0-linux64.tar.gz
+    mkdir -p ~/drivers
+    tar  -zxvf geckodriver-v0.29.0-linux64.tar.gz -C ~/drivers/
+    rm geckodriver-v0.29.0-linux64.tar.gz
+else 
+    echo "found selenuim driver - skiping download"
+fi
+
+if [ ! -f ~/.bashrc ]; then
+    echo "no bashrc file found, you will need to manauly add the path to the driver to global PATH"
+else 
+    echo "adding driver to global PATH"
+    sed -i -e '$a\' ~/.bashrc
+    echo 'export PATH=$PATH:~/drivers/' >> ~/.bashrc
+    eval `export PATH=$PATH:~/drivers/`
+fi
