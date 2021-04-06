735d304c5f8d24011e40a3308f186231fcc34128
ygreiver <yosef.g@tevel-tech.com>
2021-04-06 16:53:01 +0300

    fix bot

--- checkapoint-bot.js
+++ checkapoint-bot.js
@@ -6,22 +6,22 @@ const groupId = "-436926629";
 
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on("message", (msg) => {
-        const chatId = msg.chat.id;
+// bot.on("message", (msg) => {
+//         const chatId = msg.chat.id;
         
-        console.log("chatId:", chatId);
+//         console.log("chatId:", chatId);
         
-        bot.sendMessage(chatId, 'test');
-})
+//         bot.sendMessage(chatId, 'test');
+// })
 
 function sendMessage(msg){
         bot.sendMessage(groupId, msg)
 }
 
-function timeout(){
-        setTimeout(() => timeout(), 10000)
-}
+// function timeout(){
+//         setTimeout(() => timeout(), 10000)
+// }
 
-timeout()
+// timeout()
 
 module.exports.sendMessage = sendMessage;
