aecc67c788c4ee944f3ac4fe452498de14e588df
ygreiver <yosef.g@tevel-tech.com>
2022-03-03 14:29:52 +0200

    add ability to print msg with loading dots

--- logger.js
+++ logger.js
@@ -2,15 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const { Console } = require('console');
 const { Transform } = require('stream');
+const readline = require('readline');
 
 class Logger {
 
     constructor(){
         this.ts = new Transform({transform(chunk, enc, cb){cb(null, chunk)}});
         this.tableConsole = new Console({stdout: this.ts});
+        this.interval = undefined;
+        this.currDots = "."
     }
 
     async table(data){
+        this.endInterval();
         this.tableConsole.table(data);
         console.table(data);
         const msg = (this.ts.read() || '').toString();
@@ -18,15 +22,40 @@ class Logger {
     }
 
     async log(msg){
+        this.endInterval();
         console.log(msg);
         await this.writeToFile(msg);
     }
 
     async error(msg, save = true){
+        this.endInterval();
         console.error(msg);
 
         if(save){
             await this.writeToFile(msg)
+        }
+    }
+
+    async loadMsg(msg){
+        this.writeToFile(msg);
+        this.startInterval(msg);
+    }
+
+    startInterval(msg){        
+        this.interval = setInterval(() => {
+            this.currDots = this.currDots == "..." ? "." : this.currDots + ".";
+            readline.clearLine(process.stdout,-1)
+            readline.cursorTo(process.stdout,0);
+            process.stdout.write("\x1B[?25l") // "\x1B[?25h" to enable curser
+            process.stdout.write(`${msg}${this.currDots}`);
+        },300)
+    }
+
+    endInterval(){
+        if(this.interval){
+            clearInterval(this.interval);
+            console.log(''); // easiest way to write new line
+            this.interval = undefined;
         }
     }
 
